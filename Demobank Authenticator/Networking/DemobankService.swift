4565f205e1895d0b18355eb2e76e6b63fffec72e
Porraryd <pontus.orraryd@tink.com>
2020-06-15 14:56:28 +0200

    Add cancel and confirm endpoints

--- Demobank Authenticator/Networking/DemobankService.swift
+++ Demobank Authenticator/Networking/DemobankService.swift
@@ -9,22 +9,34 @@ final class DemobankService {
     private let baseUrl = URL(string: "https://demobank.production.global.tink.se/api")!
 
     func fetchTicket(_ ticket: String) -> AnyPublisher<Ticket, Error> {
-
         let urlRequest = URLRequest(url: baseUrl.appendingPathComponent("auth/ticket/\(ticket)"))
+        return performRequest(urlRequest: urlRequest)
+    }
 
+    func cancelTicket(_ ticket: String) -> AnyPublisher<CompleteMessage, Error> {
+        let urlRequest = URLRequest(url: baseUrl.appendingPathComponent("auth/ticket/\(ticket)/cancel"))
+        return performRequest(urlRequest: urlRequest)
+    }
+
+    func confirmTicket(_ ticket: String) -> AnyPublisher<CompleteMessage, Error> {
+        let urlRequest = URLRequest(url: baseUrl.appendingPathComponent("auth/ticket/\(ticket)/confirm"))
+        return performRequest(urlRequest: urlRequest)
+    }
+
+    private func performRequest<Object: Decodable>(urlRequest: URLRequest) -> AnyPublisher<Object, Error> {
         return urlSession.dataTaskPublisher(for: urlRequest)
-            .tryMap { element -> Data in
-                guard let response = element.response as? HTTPURLResponse, response.statusCode == 200 else {
-                    if let errorBody = try? self.decoder.decode(DemobankError.self, from: element.data) {
-                        throw errorBody
-                    } else {
-                        throw URLError(.badServerResponse)
-                    }
+        .tryMap { element -> Data in
+            guard let response = element.response as? HTTPURLResponse, response.statusCode == 200 else {
+                if let errorBody = try? self.decoder.decode(DemobankError.self, from: element.data) {
+                    throw errorBody
+                } else {
+                    throw URLError(.badServerResponse)
                 }
-                return element.data
             }
-            .decode(type: Ticket.self, decoder: decoder)
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
+            return element.data
+        }
+        .decode(type: Object.self, decoder: decoder)
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
     }
 }
