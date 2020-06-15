274796e4330e005ca9bd53357b48a46ab86c6947
Porraryd <pontus.orraryd@tink.com>
2020-06-15 14:49:34 +0200

    Remove copyright notices

--- Demobank Authenticator/AppDelegate.swift
+++ Demobank Authenticator/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Demobank Authenticator
-//
-//  Created by Pontus Orraryd on 2020-06-15.
-//  Copyright © 2020 Pontus Orraryd. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
