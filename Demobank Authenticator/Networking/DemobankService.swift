90aa9b0185e39abd53e1343a1699a339fb2b4e48
Porraryd <pontus.orraryd@tink.com>
2020-06-15 10:24:47 +0200

    Init new project

---
+++ Demobank Authenticator/Networking/DemobankService.swift
@@ -0,0 +1,15 @@
+//
+//  DemobankService.swift
+//  Demobank Authenticator
+//
+//  Created by Pontus Orraryd on 2020-06-15.
+//  Copyright © 2020 Pontus Orraryd. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+
+final class DemobankService {
+    
+}
