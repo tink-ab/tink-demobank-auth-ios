97f3d77668ae2381fae907c8ec5c1a4dd7f5b792
Pontus Orraryd <pontus.orraryd@tink.com>
2020-11-16 17:14:33 +0100

    Add color assets

---
+++ Demobank Authenticator/Theme/Color.swift
@@ -0,0 +1,11 @@
+import UIKit
+import SwiftUI
+
+extension Color {
+    static let background = Self(UIColor(named: "Background")!)
+    static let button = Self(UIColor(named: "Button")!)
+    static let buttonLabel = Self(UIColor(named: "ButtonLabel")!)
+    static let leftToSpend = Self(UIColor(named: "LeftToSpend")!)
+    static let tinkLight = Self(UIColor(named: "TinkLight")!)
+    static let tinkDark = Self(UIColor(named: "TinkDark")!)
+}
